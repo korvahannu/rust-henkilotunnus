1107aebb4878fd5302c887d9082f3dcb67bc5736
Hannu Korvala <hannu@Hannus-Mac-mini.home>
2022-10-05 08:35:00 +0300

    Added better example to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
+use std::io::{self};
+
 use rust_henkilotunnus::henkilotunnus::*;
 
 fn main() {
-    match Henkilotunnus::from(String::from("010181-900C")) {
-        Ok(hetu) => {
-            // Given henkilotunnus is valid
-            println!("{}", hetu.to_string());
-        },
-        Err(message) => {
-            // Given henkilotunnus is invalid
-            println!("{}", message);
+    println!("Hello, there! Please input a henkilotunnus to test:");
+    let mut input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            match Henkilotunnus::from(input.trim_end().to_string()) {
+                Ok(hetu) => {
+                    // Given henkilotunnus is valid
+                    println!("{}", hetu.to_string());
+                }
+                Err(message) => {
+                    // Given henkilotunnus is invalid
+                    println!("{}", message);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Oops! {}", e)
         }
     }
 }
